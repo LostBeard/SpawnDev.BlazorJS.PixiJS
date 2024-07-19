1e0dd5e44e12cdb5b1d730e3df35777073010f3d
Todd Tanner <todd@spawndev.com>
2024-07-18 23:00:56 -0400

    Updated Ticker

--- SpawnDev.BlazorJS.PixiJS/Ticker.cs
+++ SpawnDev.BlazorJS.PixiJS/Ticker.cs
@@ -16,6 +16,10 @@ namespace SpawnDev.BlazorJS.PixiJS
             /// </summary>
             /// <param name="_ref"></param>
             public Ticker(IJSInProcessObjectReference _ref) : base(_ref) { }
+            /// <summary>
+            /// Creates a new instance
+            /// </summary>
+            public Ticker() : base(JS.New("PIXI.Ticker")) { }
             #endregion
             #region Properties
             /// <summary>
@@ -24,6 +28,40 @@ namespace SpawnDev.BlazorJS.PixiJS
             /// Note: The cap may be exceeded by scaling.
             /// </summary>
             public float DeltaTime => JSRef!.Get<float>("deltaTime");
+            /// <summary>
+            /// The frames per second at which this ticker is running. The default is approximately 60 in most modern browsers. *** Note:** This does not factor in the value of {@link ticker.Ticker#speed|speed}, which is specific to scaling { @link ticker.Ticker#deltaTime|deltaTime}.
+            /// </summary>
+            public float FPS => JSRef!.Get<float>("FPS");
+            /// <summary>
+            /// The number of listeners on this ticker, calculated by walking through linked list
+            /// </summary>
+            public int Count => JSRef!.Get<int>("count");
+            /// <summary>
+            /// Whether or not this ticker should invoke the method {@link ticker.Ticker#start|start} automatically when a listener is added.
+            /// </summary>
+            public bool AutoStart{ get => JSRef!.Get<bool>("autoStart"); set => JSRef!.Set("autoStart", value); }
+            /// <summary>
+            /// Scaler time elapsed in milliseconds from last frame to this frame. This value is capped by setting {@link ticker.Ticker#minFPS|minFPS} and is scaled with { @link ticker.Ticker#speed|speed}. *** Note:**The cap may be exceeded by scaling. If the platform supports DOMHighResTimeStamp, this value will have a precision of 1 µs.<br/> Defaults to target frame time<br/>@default 16.66
+            /// </summary>
+            public float DeltaMS { get => JSRef!.Get<float>("deltaMS"); set => JSRef!.Set("deltaMS", value); }
+            /// <summary>
+            /// Time elapsed in milliseconds from last frame to this frame. Opposed to what the scalar {@link ticker.Ticker#deltaTime|deltaTime} is based, this value is neither capped nor scaled. If the platform supports DOMHighResTimeStamp, this value will have a precision of 1 µs. Defaults to target frame time @default 16.66
+            /// </summary>
+            public float ElapsedMS { get => JSRef!.Get<float>("elapsedMS"); set => JSRef!.Set("elapsedMS", value); }
+            /// <summary>
+            /// The last time {@link ticker.Ticker#update|update} was invoked. This value is also reset internally outside of invoking update, but only when a new animation frame is requested. If the platform supports DOMHighResTimeStamp, this value will have a precision of 1 µs.
+            /// </summary>
+            public float LastTime { get => JSRef!.Get<float>("lastTime"); set => JSRef!.Set("lastTime", value); }
+            /// <summary>
+            /// Factor of current {@link ticker.Ticker#deltaTime|deltaTime}.<br/>
+            /// @example<br/>
+            /// Scales ticker.deltaTime to what would be the equivalent of approximately 120 FPS ticker.speed = 2;
+            /// </summary>
+            public float Speed { get => JSRef!.Get<float>("speed"); set => JSRef!.Set("speed", value); }
+            /// <summary>
+            /// Whether or not this ticker has been started. `true` if {@link ticker.Ticker#start|start} has been called. `false` if { @link ticker.Ticker#stop|Stop} has been called. While `false`, this value may change to `true` in the event of { @link ticker.Ticker#autoStart|autoStart} being `true` and a listener is added.
+            /// </summary>
+            public bool Started { get => JSRef!.Get<bool>("started"); set => JSRef!.Set("started", value); }
             #endregion
             #region Methods
             /// <summary>
